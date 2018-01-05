sergiogonzalez/liferay-portal

LPS-75955 Autogenerated

modules/apps/collaboration/message-boards/message-boards-api/src/main/java/com/liferay/message/boards/service/MBDiscussionLocalServiceUtil.java
@@ -227,6 +227,12 @@ public class MBDiscussionLocalServiceUtil {
 	}
 
 	public static com.liferay.message.boards.model.MBDiscussion getDiscussion(
+		long classNameId, long classPK)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return getService().getDiscussion(classNameId, classPK);
+	}
+
+	public static com.liferay.message.boards.model.MBDiscussion getDiscussion(
 		java.lang.String className, long classPK)
 		throws com.liferay.portal.kernel.exception.PortalException {
 		return getService().getDiscussion(className, classPK);

modules/apps/collaboration/message-boards/message-boards-api/src/main/java/com/liferay/message/boards/service/MBDiscussionLocalServiceWrapper.java
@@ -240,6 +240,13 @@ public class MBDiscussionLocalServiceWrapper implements MBDiscussionLocalService
 
 	@Override
 	public com.liferay.message.boards.model.MBDiscussion getDiscussion(
+		long classNameId, long classPK)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return _mbDiscussionLocalService.getDiscussion(classNameId, classPK);
+	}
+
+	@Override
+	public com.liferay.message.boards.model.MBDiscussion getDiscussion(
 		java.lang.String className, long classPK)
 		throws com.liferay.portal.kernel.exception.PortalException {
 		return _mbDiscussionLocalService.getDiscussion(className, classPK);

Pandekh/com-liferay-message-boards

message-boards-api/src/main/java/com/liferay/message/boards/service/MBDiscussionLocalServiceUtil.java
@@ -190,6 +190,11 @@ public class MBDiscussionLocalServiceUtil {
 	}
 
 	public static com.liferay.message.boards.model.MBDiscussion fetchDiscussion(
+		long classNameId, long classPK) {
+		return getService().fetchDiscussion(classNameId, classPK);
+	}
+
+	public static com.liferay.message.boards.model.MBDiscussion fetchDiscussion(
 		java.lang.String className, long classPK) {
 		return getService().fetchDiscussion(className, classPK);
 	}

message-boards-api/src/main/java/com/liferay/message/boards/service/MBDiscussionLocalServiceWrapper.java
@@ -196,6 +196,12 @@ public class MBDiscussionLocalServiceWrapper implements MBDiscussionLocalService
 
 	@Override
 	public com.liferay.message.boards.model.MBDiscussion fetchDiscussion(
+		long classNameId, long classPK) {
+		return _mbDiscussionLocalService.fetchDiscussion(classNameId, classPK);
+	}
+
+	@Override
+	public com.liferay.message.boards.model.MBDiscussion fetchDiscussion(
 		java.lang.String className, long classPK) {
 		return _mbDiscussionLocalService.fetchDiscussion(className, classPK);
 	}